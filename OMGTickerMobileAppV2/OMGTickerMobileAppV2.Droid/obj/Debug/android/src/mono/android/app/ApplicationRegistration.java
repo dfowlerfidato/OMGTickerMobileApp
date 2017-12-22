297db249e60afb0483f423af4f851103fd71fae6
dfowlerfidato <33792548+dfowlerfidato@users.noreply.github.com>
2017-12-22 13:27:03 -0800

    Added all the files for mobile notification app

---
+++ OMGTickerMobileAppV2/OMGTickerMobileAppV2.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("OMGTickerMobileAppV2.Droid.MainApplication, OMGNotificationApp.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md554c46139b43c6b359a3ac5f56c59c776.MainApplication.class, md554c46139b43c6b359a3ac5f56c59c776.MainApplication.__md_methods);
+		
+	}
+}
---
+++ OMGTickerMobileAppV2/OMGTickerMobileAppV2/OMGTickerMobileAppV2.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("OMGTickerMobileAppV2.Droid.MainApplication, OMGTickerMobileAppV2.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md59341398f9a7134627c09dcfc7fd3a175.MainApplication.class, md59341398f9a7134627c09dcfc7fd3a175.MainApplication.__md_methods);
+		
+	}
+}
